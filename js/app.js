e267d3ef45043f2f9ec75dfdb3e04743de825e6c
Michal Škoula <imichalthemichal@gmail.com>
2024-06-04 19:36:09 +0200

    Add 'Add new food' form and CSS

--- js/app.js
+++ js/app.js
@@ -9,12 +9,42 @@ profileOpenButton.addEventListener('click', (e) => {
 
 const filterBtn = document.getElementById('filter-btn');
 const filterNav = document.getElementById('filter-nav');
-// const input = document.querySelector('.input');
 
 filterBtn.addEventListener('click', (e) => {
 
-  // input.classList.toggle('border-adjusted')
   filterNav.classList.toggle('open-top');
 
+});
+
+const addNewBtn = document.getElementById('add-new-btn');
+addNewBtn.addEventListener('click', (e) => {
+
+  location.href = 'new-food-form.html'; // will be changed
 
 });
+
+
+
+
+
+
+// Code for closing menus when clicking off, havent finished it yet
+
+// const targetElement = document.getElementById('targetElement');
+// const excludedElements = [
+//       document.getElementById('excludedElement'),
+//       document.getElementById('excludedButton')
+//   ];
+
+//   document.addEventListener('click', (event) => {
+//       // Check if the clicked element is one of the excluded elements
+//       const isClickInsideExcluded = excludedElements.some(element => element.contains(event.target));
+
+//       // Check if the click is inside the target element
+//       const isClickInsideTarget = targetElement.contains(event.target);
+
+//       // Toggle the class if the click is outside the excluded and target elements
+//       if (!isClickInsideExcluded && !isClickInsideTarget) {
+//           targetElement.classList.toggle('highlight');
+//       }
+//   });
